535f7a5e3c674c5bc8ce1556ca8cede17ec2ce31
Ahmet <53380248+Ahmet-Dev@users.noreply.github.com>
2020-09-01 00:57:04 +0300

    Update Scoring.cs

--- Scoring.cs
+++ Scoring.cs
@@ -8,19 +8,13 @@ public class Scoring : MonoBehaviour {
     public static int score = 0;
     public Text scoreText;
     public Text coinText;
-	// Use this for initialization
 	void Start () {
-
-        //set the initial score
         score = 0;
-        //set the coins, previously stored 
         coinText.text = GameController.getCoins().ToString();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-        //keeps updating scores
         scoreText.text = score.ToString();
         coinText.text = GameController.getCoins().ToString();
 	}
