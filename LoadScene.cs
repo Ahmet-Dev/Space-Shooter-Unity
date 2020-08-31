9d9a30036316f6dc656045c54d747b70b985ed48
Ahmet <53380248+Ahmet-Dev@users.noreply.github.com>
2020-09-01 00:53:03 +0300

    Update LoadScene.cs

--- LoadScene.cs
+++ LoadScene.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour {
-
     public string scene;
-
-    //takes a scene name and load the scene
     public void loadScene(string scene){
-
         Time.timeScale = 1;
         SceneManager.LoadScene(scene);
-
     }
 }
