5e928f970ef5f4898cf86980466b3da4874c206f
Ahmet <53380248+Ahmet-Dev@users.noreply.github.com>
2020-09-01 00:56:44 +0300

    Update RocketModel.cs

--- RocketModel.cs
+++ RocketModel.cs
@@ -9,25 +9,19 @@ public class RocketModel: MonoBehaviour {
     public int price;
     public int rocketId;
     private int status;
-
-    //Constructor 
     public RocketModel(){
         rocketSprite = null;
 
         price = 0;
     }
 
-    // Sprite getter method
     public Sprite getSprite(){
         return rocketSprite;
     }
 
-    //Sprite setter method
     public int getPrice(){
         return price;
     }
-
-    //Returns an integer, which indicates if the rocket is bought or not
     public int getStatus(){
         
         if (PlayerPrefs.GetInt("RocketBuy" + rocketId.ToString()) == 0)
@@ -43,8 +37,6 @@ public class RocketModel: MonoBehaviour {
 
         return status;
     }
-
-    //Set status
     public void setStatus(int rocketId){
         PlayerPrefs.SetInt("RocketBuy" + rocketId.ToString(), 1);
     }
