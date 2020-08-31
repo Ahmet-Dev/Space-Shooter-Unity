d6d6fc57bbf7b421d7716b110be8ce818df379e0
Ahmet <53380248+Ahmet-Dev@users.noreply.github.com>
2020-09-01 00:48:25 +0300

    Update AdmobVideo.cs

--- AdmobVideo.cs
+++ AdmobVideo.cs
@@ -24,31 +24,26 @@ public class AdmobVideo : MonoBehaviour {
 
     public void Start()
     {
-        // Get singleton reward based video ad reference.
+
         this.rewardBasedVideo = RewardBasedVideoAd.Instance;
 
-        // Called when an ad request has successfully loaded.
         rewardBasedVideo.OnAdLoaded += HandleRewardBasedVideoLoaded;
-        // Called when an ad request failed to load.
+ 
         rewardBasedVideo.OnAdFailedToLoad += HandleRewardBasedVideoFailedToLoad;
-         //Called when an ad is shown.
+ 
 
         rewardBasedVideo.OnAdOpening += HandleRewardBasedVideoOpened;
-        //// Called when the ad starts to play.
-        //rewardBasedVideo.OnAdStarted += HandleRewardBasedVideoStarted;
-        //// Called when the user should be rewarded for watching a video.
+
         rewardBasedVideo.OnAdRewarded += HandleRewardBasedVideoRewarded;
-        // Called when the ad is closed.
+
         rewardBasedVideo.OnAdClosed += HandleRewardBasedVideoClosed;
-        //// Called when the ad click caused the user to leave the application.
-        //rewardBasedVideo.OnAdLeavingApplication += HandleRewardBasedVideoLeftApplication;
-        //Request for a reward based video
+
         this.RequestRewardBasedVideo();
     }
 
 	private void Update()
 	{
-        //whenever a video is loaded, set isLoaded true
+
         if (rewardBasedVideo.IsLoaded())
         {
             isAdLoaded = true;
@@ -58,7 +53,7 @@ public class AdmobVideo : MonoBehaviour {
 
 	public void RequestRewardBasedVideo()
     {
-        //Set your ad unit id here
+
         #if UNITY_ANDROID
             string adUnitId = "ca-app-pub-3940256099942544/5224354917";
         #elif UNITY_IPHONE
@@ -67,16 +62,15 @@ public class AdmobVideo : MonoBehaviour {
             string adUnitId = "unexpected_platform";
         #endif
 
-        // Create an empty ad request.
         AdRequest request = new AdRequest.Builder().Build();
-        // Load the rewarded video ad with the request.
+
         this.rewardBasedVideo.LoadAd(request, adUnitId);
 
        
     }
 
 
-    //this method is called whren the loaded video needs to be shown
+
     public void adShow(){
         
         if (isAdLoaded)
@@ -101,7 +95,7 @@ public class AdmobVideo : MonoBehaviour {
 
     public void HandleRewardBasedVideoClosed(object sender, EventArgs args)
     {
-        //if the video is not watched fully, the user won't get any life
+
 
         if(!isRewarded){
             PlayerMove.instance.onCancel();
@@ -112,14 +106,14 @@ public class AdmobVideo : MonoBehaviour {
 
     public void HandleRewardBasedVideoRewarded(object sender, Reward args)
     {
-        //if the video is watched fully and rewarded, then set the bool true, and add some life
+
         isRewarded = true;
         addLife();
     }
 
     public void HandleRewardBasedVideoOpened(object sender, EventArgs args)
     {
-        //close the panel when the video is playing 
+
         continuePanel.SetActive(false);
         isAdLoaded = false;
     }
