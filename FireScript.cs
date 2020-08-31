255538e12c91c8c62b3a0803afd8bf3d21666712
Ahmet <53380248+Ahmet-Dev@users.noreply.github.com>
2020-09-01 00:52:16 +0300

    Update FireScript.cs

--- FireScript.cs
+++ FireScript.cs
@@ -3,14 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FireScript : MonoBehaviour {
-
-    //Assign the bullet prefab
 	public GameObject bulletPrefab;
-    //Assign combinations of enemies
 	public GameObject[] enemyPrefab;
-
-    // the point from where the enemies are spawned
-    public Transform enemySpawn;
+    	public Transform enemySpawn;
 
     int x;
 	float time;
@@ -24,14 +19,9 @@ public class FireScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-		
-        //Takes a random combination to spawn
         x = Random.Range(0, enemyPrefab.Length -1);
 		//yPosition = 8;
 		time += Time.deltaTime;
-
-
-        //every enemy will be created after 2 sec 
 		if(time >= 2){
 
             var enemy = Instantiate(enemyPrefab[x], enemySpawn.position, enemySpawn.rotation);
@@ -41,12 +31,7 @@ public class FireScript : MonoBehaviour {
 
 
 		}
-		 
-		
-
-
 
 	}
-
 	
 }
